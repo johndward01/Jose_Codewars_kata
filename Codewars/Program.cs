68d2301a854adfbf48613d0b98d9a194a31f40ef
johndward01 <johndward01@gmail.com>
2023-01-27 16:26:53 -0600

    Add project files.

---
+++ Codewars/Program.cs
@@ -0,0 +1,18 @@
+﻿var height = Height(7);
+Console.WriteLine(height);
+
+
+static string Height(int n)
+{
+    double catInHat = 2_000_000;
+    double sum = 2_000_000;
+    if (n == 0)
+    {
+        return catInHat.ToString();
+    }
+
+    sum += double.Parse(Height(n - 1)) / 2.5 ;
+
+    return $"{sum:N3}";
+
+}
